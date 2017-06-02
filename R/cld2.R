5ef463f53c3df6318931603662bf482d74b69c1f
Jeroen Ooms <jeroenooms@gmail.com>
2017-06-03 00:19:26 +0200

    first commit

---
+++ R/cld2.R
@@ -0,0 +1,25 @@
+#' Compact Language Detector 2
+#'
+#' The function [detect_language()] function returns the best guess of the language of
+#' `text` or `NA` if the language could not reliablity be determinted.
+#' The function [detect_language_multi()] returns the top 3 candidate languages with
+#' a probability score, and also the total number of text bytes that was succesfully
+#' classified.
+#'
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib cld2
+#' @export
+#' @rdname cld2
+#' @param text a string with text to guess.
+#' @param plain_text if `FALSE` then code skips HTML tags and expands HTML entities
+detect_language <- function(text, plain_text = TRUE){
+  text <- paste(text, collapse = "\n")
+  detect_language_cc(text, plain_text)
+}
+
+#' @export
+#' @rdname cld2
+detect_language_multi <- function(text, plain_text = TRUE){
+  text <- paste(text, collapse = "\n")
+  detect_language_multi_cc(text, plain_text)
+}
---
+++ src/wrapper.cpp
@@ -0,0 +1,54 @@
+#define STRICT_R_HEADERS
+#define R_NO_REMAP
+
+#include <Rcpp.h>
+#include "libcld2/public/compact_lang_det.h"
+
+/* Super simple api, returns language as string if pretty certain, otherwise NA */
+
+// [[Rcpp::export]]
+Rcpp::CharacterVector detect_language_cc(Rcpp::String input, bool plain_text = true){
+  input.set_encoding(CE_UTF8);
+  std::string buf(input);
+  bool is_reliable;
+  CLD2::Language lang = CLD2::DetectLanguage(buf.c_str(), buf.length(), plain_text, &is_reliable);
+  if(!is_reliable)
+    return NA_STRING;
+  Rcpp::CharacterVector out(LanguageName(lang));
+  out.attr("code") = Rcpp::String(LanguageCode(lang));
+  return out;
+}
+
+/* Pro API: return all data */
+
+// [[Rcpp::export]]
+Rcpp::List detect_language_multi_cc(Rcpp::String input, bool plain_text = true){
+  input.set_encoding(CE_UTF8);
+  std::string buf(input);
+  bool is_reliable;
+  CLD2::Language lang[3];
+  int percent[3];
+  int text_bytes;
+  CLD2::DetectLanguageSummary(buf.c_str(), buf.length(), plain_text, lang, percent, &text_bytes, &is_reliable);
+  Rcpp::CharacterVector langs;
+  Rcpp::CharacterVector codes;
+  Rcpp::LogicalVector latin;
+  Rcpp::NumericVector pct;
+  for(int i = 0; i < 3; i++){
+    langs.push_back(LanguageName(lang[i]));
+    codes.push_back(LanguageCode(lang[i]));
+    latin.push_back(IsLatnLanguage(lang[i]));
+    pct.push_back(percent[i]);
+  }
+  Rcpp::DataFrame guesses = Rcpp::DataFrame::create(
+    Rcpp::Named("language") = langs,
+    Rcpp::Named("code") = codes,
+    Rcpp::Named("latin") = latin,
+    Rcpp::Named("probability") = pct / 100
+  );
+  return Rcpp::List::create(
+    Rcpp::Named("guess") = guesses,
+    Rcpp::Named("bytes") = text_bytes,
+    Rcpp::Named("reliabale") = is_reliable
+  );
+}
