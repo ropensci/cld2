638e287bf7d72037a4a544a30b73230ecceceaea
Jeroen Ooms <jeroenooms@gmail.com>
2017-06-03 01:09:36 +0200

    Typos

--- R/cld2.R
+++ R/cld2.R
@@ -1,10 +1,9 @@
 #' Compact Language Detector 2
 #'
-#' The function [detect_language()] function returns the best guess of the language of
-#' `text` or `NA` if the language could not reliablity be determinted.
-#' The function [detect_language_multi()] returns the top 3 candidate languages with
-#' a probability score, and also the total number of text bytes that was succesfully
-#' classified.
+#' The function [detect_language()] guesses the language of `text` or returns `NA`
+#' if the language could not reliably be determined. The function [detect_language_multi()]
+#' shows the top 3 guessed languages including a relative probability score, and also shows
+#' the total number of text bytes that was reliably classified into either language.
 #'
 #' @importFrom Rcpp sourceCpp
 #' @useDynLib cld2
